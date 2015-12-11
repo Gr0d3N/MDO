f11e9247f751d837c65eba90c189b6a5ba4d6b48
Mina Habib <mina@sdf.org>
2015-12-10 20:57:23 -0600

    Fix the code to take a boolean for symbols

--- MDO.py
+++ MDO.py
@@ -82,21 +82,21 @@ ALPHABET = ('abcdefghijklmnopqrstuvwxyz'
             'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
             '0123456789!@#$%^&*()-_')
 
-def password(plaintext, service, length=10):
+def password(plaintext, service, length=10, alphabet=ALPHABET):
     raw_hexdigest = make_password(plaintext, service)
 
     # Convert the hexdigest into decimal
     num = int(raw_hexdigest, 16)
 
     # What base will we convert `num` into?
-    num_chars = len(ALPHABET)
+    num_chars = len(alphabet)
 
     # Build up the new password one "digit" at a time,
     # up to a certain length
     chars = []
     while len(chars) < length:
         num, idx = divmod(num, num_chars)
-        chars.append(ALPHABET[idx])
+        chars.append(alphabet[idx])
 
     return ''.join(chars)
 
@@ -124,12 +124,12 @@ class Service(Model):
         return alpha
 
     def password(self, plaintext):
-        print_password = password(plaintext, self.name, self.length)
+        print_password = password(plaintext, self.name, self.length, self.get_alphabet())
         print "Your password is:"
         print print_password
 
     def c_password(self, plaintext):
-        c_password = password(plaintext, self.name, self.length)
+        c_password = password(plaintext, self.name, self.length, self.get_alphabet())
         r = Tk()
         r.withdraw()
         r.clipboard_clear()
